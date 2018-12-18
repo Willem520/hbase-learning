a914df54532b382334307798398c81e0c97e0109
weiyu <weiyu005@ke.com>
2018-12-18 16:38:18 +0800

    修改文件

--- src/test/java/willem/weiyu/bigData/hbase/EasyHBaseClientTest.java
+++ src/test/java/willem/weiyu/bigData/hbase/EasyHBaseClientTest.java
@@ -2,7 +2,13 @@ package willem.weiyu.bigData.hbase;
 
 import org.junit.Test;
 
+import java.io.File;
 import java.io.IOException;
+import java.text.DecimalFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.Random;
 
 /**
  * @author weiyu
@@ -29,7 +35,16 @@ public class EasyHBaseClientTest {
 
     @Test
     public void testAdd() throws IOException {
-        EasyHBaseClient.Builder.create().add("weiyu_hbase","0001","fm1","name","weiyu");
+        EasyHBaseClient  hBaseClient = EasyHBaseClient.Builder.create();
+        Random rand = new Random();
+        String separator = "_";
+        DecimalFormat df = new DecimalFormat("0.00");
+        for (int i=1; i<=5; i++){
+            String key = "000"+i;
+            key = key.hashCode() + separator +"20181218"+ separator + key;
+            hBaseClient.add("test",key,"fm1","score",df.format(rand.nextDouble()*100));
+            hBaseClient.add("test",key,"fm2","rank",i+"");
+        }
     }
 
     @Test
